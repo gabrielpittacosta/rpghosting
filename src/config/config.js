b5afa7c6dbec9063699a7520681156e0a9cb52c2
gabrielpittacosta <gabrielpittacosta@gmail.com>
2019-10-12 16:10:12 -0300

    correção das variaveies de ambiente

--- src/config/config.js
+++ src/config/config.js
@@ -4,10 +4,7 @@ module.exports = {
     database: process.env.POSTGRES_DB,
     username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
-    host: '172.18.0.2', //docker ambiente       
-    // host: '10.0.0.9', //producao ambiente      
-    //host: 'host.docker.internal',
-    //host: 'ec2-107-20-168-237.compute-1.amazonaws.com',
+    host: process.env.HOST,
     dialect: 'postgres'
   }
 }
